dcf0acc1a3cc37e1a75921f77ba10815be493735
Andy Glassman <aglassman@digitalmeasures.com>
2016-09-22 22:38:16 -0500

    Changed to tags.

--- src/main/java/org/gmjm/web/CoffeeBotController.java
+++ src/main/java/org/gmjm/web/CoffeeBotController.java
@@ -1,5 +1,6 @@
 package org.gmjm.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,12 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CoffeeBotController
 {
+
+	@Value("app.version")
+	private String version;
+
 	@RequestMapping("/hello")
 	public String sayHello(@RequestParam String name) {
 		Assert.notNull(name);
 		return "Hello new2: " + name.trim()
 			.substring(0,
 				name.length() >= 20 ? 20 : name.length());
+	}
+
+	@RequestMapping("/version")
+	public String version() {
+		return "coffee-bot-" + version;
 	}
 
 }
