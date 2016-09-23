7b0f1f353758230ed380451d5caca6bf6801fb36
Andy Glassman <aglassman@digitalmeasures.com>
2016-09-22 23:00:11 -0500

    Fixed property replacement

--- src/main/java/org/gmjm/web/CoffeeBotController.java
+++ src/main/java/org/gmjm/web/CoffeeBotController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CoffeeBotController
 {
 
-	@Value("app.version")
+	@Value("${app.version}")
 	private String version;
 
 	@RequestMapping("/hello")
